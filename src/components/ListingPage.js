003702ea4291ea8eeb71ffb8f96b239327a8c39b
Sajood Ur Rehman <sajood415@gmail.com>
2024-01-23 16:15:01 +0500

    Added sorting/searching functionality

--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -7,6 +7,8 @@ import NewItemModal from './NewItem';
 const ListingPage = () => {
   const [items, setItems] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOption, setSortOption] = useState('default');
 
   const handleAddItem = async (newItem) => {
     try {
@@ -26,20 +28,71 @@ const ListingPage = () => {
     }
   };
 
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSort = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  const filterAndSortItems = () => {
+    let filteredItems = items;
+
+    if (searchTerm) {
+      filteredItems = filteredItems.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    switch (sortOption) {
+      case 'lowToHigh':
+        filteredItems.sort((a, b) => a.price - b.price);
+        break;
+      case 'highToLow':
+        filteredItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+
+    return filteredItems;
+  };
+
   useEffect(() => {
     fetchItems();
   }, []);
 
+  const filteredAndSortedItems = filterAndSortItems();
+
   return (
     <div>
       <Navbar showCartIcon={true} showAddItemButton={true} showModal={() => setModalOpen(true)} />
-      <div className="item-grid">
-        {items.map((item) => (
-          <div key={item.id} className="grid-item">
-            <Item item={item} />
-          </div>
-        ))}
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Search by item name..."
+          value={searchTerm}
+          onChange={handleSearch}
+          className="search-input"
+        />
+        <select value={sortOption} onChange={handleSort} className="sort-dropdown">
+          <option value="default">Sort by</option>
+          <option value="lowToHigh">Price: Low to High</option>
+          <option value="highToLow">Price: High to Low</option>
+        </select>
       </div>
+      {filteredAndSortedItems.length === 0 ? (
+        <div className="no-items-message">Oops... No items were found :(</div>
+      ) : (
+        <div className="item-grid">
+          {filteredAndSortedItems.map((item) => (
+            <div key={item.id} className="grid-item">
+              <Item item={item} />
+            </div>
+          ))}
+        </div>
+      )}
       <NewItemModal isOpen={isModalOpen} closeModal={() => setModalOpen(false)} onAddItem={handleAddItem} />
     </div>
   );
